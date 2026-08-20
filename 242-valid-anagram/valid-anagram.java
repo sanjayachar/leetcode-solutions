5d4cf2df67b337fb90b51bae77c0087b48358ea1
sanjayachar <76770449+sanjayachar@users.noreply.github.com>
2026-08-20 07:28:38 +0530

    Time: 4 ms (83.06%) | Memory: 46.4 MB (34.82%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -6,13 +6,9 @@ class Solution {
         char[]ch2=t.toCharArray();
         Arrays.sort(ch1);
         Arrays.sort(ch2);
-        String s1 = new String(ch1);
-        String s2 = new String(ch2);
-        for(int i=0; i<t.length(); i++) {
-            if(s1.charAt(i) != s2.charAt(i)){
-                return false;
-            }
+        if(Arrays.equals(ch1,ch2)){
+            return true;
         }
-        return true;
+        return false;
     }
 }
